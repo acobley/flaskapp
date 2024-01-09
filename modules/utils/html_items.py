43aeb8d52554fedd6de07a5356ac395305b2f373
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 20:24:23 +0000

    Update html_items.py

--- modules/utils/html_items.py
+++ modules/utils/html_items.py
@@ -1,7 +1,16 @@
-def carouselItem(src, alt, isActive) -> str: 
+def carouselItem(src, alt, isActive: bool) -> str: 
     element = ''
     if (isActive):
         element = ' <div class="carousel-item active">\n\t<img src="'+ str(src) +'" class="d-block w-100" alt="'+str(alt)+'">\n</div>'
     else:
         element = ' <div class="carousel-item">\n\t<img src="'+ str(src) +'" class="d-block w-100" alt="'+str(alt)+'">\n</div>'
+    return element
+
+def carouselIndicator(data_target_id:str, index:int, isActive: bool) -> str:
+    element = ''
+    
+    if (isActive):
+        element = '<button type="button" data-bs-target="#'+str(data_target_id)+'" data-bs-slide-to="'+str(index)+'" class="active" aria-current="true" aria-label="Slide '+str(index+1)+'"></button>'
+    else:
+        element = '<button type="button" data-bs-target="#'+str(data_target_id)+'" data-bs-slide-to="'+str(index)+'" aria-label="Slide '+str(index+1)+'"></button>'
     return element
