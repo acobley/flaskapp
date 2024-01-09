d0f5a9d9a011611b6f1297d6322ab285ad6aab30
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 22:27:59 +0000

    Update index.py

--- index.py
+++ index.py
@@ -38,7 +38,7 @@ def index_page():
 def video_page(uuid):
     log.LOG_MESSAGE('['+request.remote_addr+'] --> ' + uuid)
     
-    URL_UUID_FILTER = '{0}?filter={"video.uuid":""}'.format(glb.URL_VIDEOS, uuid)
+    URL_UUID_FILTER = '{0}?filter={"video.uuid":"{1}"}'.format(glb.URL_VIDEOS, uuid)
     response = requests.get(URL_UUID_FILTER)    
     if (response.status_code != 200):
         log.LOG_ERROR("Unexpected response: {0}. Status: {1}. Message: {2}".format(response.reason, response.status, video['Exception']['Message']))
@@ -47,7 +47,7 @@ def video_page(uuid):
         log.LOG_SUCCESS("[{0}] -- Fetched correctly!".format(request.remote_addr))
         
     video = json.dumps(response.json(), index=4)
-    print(video)
+    log.LOG_TRACE(video)
     
     return uuid
 
