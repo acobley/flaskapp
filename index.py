d798247210afd169bda316879c799c4ca7c5c4c0
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 16:17:48 +0000

    Template test

--- index.py
+++ index.py
@@ -4,9 +4,16 @@ from flask import Flask, request, session, g, redirect, url_for, abort, render_t
 app = Flask(__name__)
 app.debug = True
 
+IP_DB = "34.16.159.36"
+IP_NGINX = "34.125.25.53"
+
 @app.route('/')
 def hello_world():
     return '<h1>Hello World!</h1>'
+
+@app.route('myflix/index.html')
+def name_page():
+    return render_template('index.html', name=IP_DB)
 
 @app.route('/Hello/<name>')
 def hello_page(name):
