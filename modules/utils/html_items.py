8947a6e02977fae43b551657c42a8993c1db4ffe
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 20:13:44 +0000

    Disabled active

--- modules/utils/html_items.py
+++ modules/utils/html_items.py
@@ -1,2 +1,2 @@
 def carouselItem(src, alt) -> str:
-    return ' <div class="carousel-item active">\n\t<img src="'+ str(src) +'" class="d-block w-100" alt="'+str(alt)+'">\n</div>'
+    return ' <div class="carousel-item">\n\t<img src="'+ str(src) +'" class="d-block w-100" alt="'+str(alt)+'">\n</div>'
