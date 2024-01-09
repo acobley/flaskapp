c9e99815b1b29bccc1aa44fc8b80b4613bbba151
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 18:09:10 +0000

    Test

--- index.py
+++ index.py
@@ -7,6 +7,31 @@ app.debug = True
 IP_DB = "34.16.159.36"
 IP_NGINX = "34.125.25.53"
 
+HTML = """ <!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
+        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+   
+    <link href="https://vjs.zencdn.net/8.9.0/video-js.css" rel="stylesheet" />
+    <script src="https://vjs.zencdn.net/8.9.0/video.min.js"></script>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>myFlix</title>
+</head> 
+
+<body>
+    <a href="/"><h1>myFlix</h1></a>
+    <a href="/Video/Big-Buck-Bunny"><p> Movie </p></a>
+
+</body>
+
+</html>
+"""
+
 @app.route('/')
 def hello_world():
     
@@ -14,9 +39,9 @@ def hello_world():
     
     return render_template('index.html', video_name=IP_NGINX, video_src=src)
 
-@app.route('/Video/<name>')
+@app.route('/Test/')
 def hello_page(name):
-    return '<h1>Hello '+name+'</h1>'
+    return HTML
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
