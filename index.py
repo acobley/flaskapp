8fc03e9a39e38eb52f771df08dd65423a8ebe362
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 19:13:12 +0000

    Major update

--- index.py
+++ index.py
@@ -2,37 +2,29 @@ import requests
 import json
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash
 
+# Custom modules
+import modules.data.globals as glb
+
 app = Flask(__name__)
 app.debug = True
-
-IP_DB = "34.16.159.36"
-IP_NGINX = "34.125.25.53"
-
-TEST = [
-    1,2,3,4,5,6,7,8,9,10
-]
 
 @app.route('/')
 def hello_world():
     
-
+    src = "http://" + glb.IP_NGINX + "/mp4/bbb.mp4"
     
-    src = "http://" + IP_NGINX + "/mp4/bbb.mp4"
-    
-    return render_template('index.html', video_name=IP_NGINX, video_src=src)
+    return render_template('index.html', video_name=glb.IP_NGINX, video_src=src)
 
 @app.route('/Test/')
 def hello_page():
     
-    URL_CATEGORIES = "http://34.16.159.36/myflix/categories"
-    URL_VIDEOS = "http://34.16.159.36/myflix/videos"
-    
-    response = requests.get(URL_CATEGORIES)
+    response = requests.get(glb.URL_CATEGORIES)
     print(response)
+    
     JSON_CATEGORIES = response.json()
     CATEGORIES = [item['category'] for item in JSON_CATEGORIES]
     
-    response = requests.get(URL_VIDEOS)
+    response = requests.get(glb.URL_VIDEOS)
     print(response)
     JSON_VIDEOS = response.json()
     VIDEOS = [item['video']['Name'] for item in JSON_VIDEOS]
---
+++ modules/data/globals.py
@@ -0,0 +1,7 @@
+# Server IPs
+IP_DB = "34.16.159.36"
+IP_NGINX = "34.125.25.53"
+
+# DB Tables
+URL_CATEGORIES = "http://" +IP_DB+ "/myflix/categories"
+URL_VIDEOS = "http://" +IP_DB+ "/myflix/videos"
