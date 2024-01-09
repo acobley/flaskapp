5cc8c7099285f862e5a4dcb849f74a5d84d93c35
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-09 18:21:06 +0000

    List test

--- index.py
+++ index.py
@@ -7,31 +7,9 @@ app.debug = True
 IP_DB = "34.16.159.36"
 IP_NGINX = "34.125.25.53"
 
-TEST = """
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-        integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-   
-    <link href="https://vjs.zencdn.net/8.9.0/video-js.css" rel="stylesheet" />
-    <script src="https://vjs.zencdn.net/8.9.0/video.min.js"></script>
-
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <title>myFlix</title>
-</head>
-
-<body>
-    <a href="/"><h1>myFlix</h1></a>
-    <a href="/Video/Big-Buck-Bunny"><p> Movie </p></a>
-
-</body>
-
-</html>
-"""
+TEST = [
+    1,2,3,4,5,6,7,8,9,10
+]
 
 @app.route('/')
 def hello_world():
@@ -43,7 +21,12 @@ def hello_world():
 @app.route('/Test/')
 def hello_page():
     
-    return TEST
+    names = []
+    for number in TEST:
+        names.append( "<li> " +number+ " </li>" )
+        
+    
+    return render_template('index.html', items=names)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0',port=80)
