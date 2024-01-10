ca95d3398cad610415e2147c9e724aeed1b9e00d
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-10 18:37:07 +0000

    Updated syntax

--- modules/utils/html_items.py
+++ modules/utils/html_items.py
@@ -1,16 +1,15 @@
 def column_card(title:str, href:str, imgsrc:str) -> str:
         
-    elem = '\
-    <div class="col-sm-12 col-md-12 col-lg-3 d-flex mt-5 mb-1 justify-content-center"> \
-        <div class="card" style="width: 24rem;">\
-            <a href="/Videos/'+href+'">\
-                <img src="'+imgsrc+'" class="card-img-top" alt="'+title+'"> \
-            </a>\
-            <div class="card-body">\
-                <h5 class="card-title">'+title+'</h5>\
+    elem = '<div class="col-sm-12 col-md-12 col-lg-3 d-flex mt-5 mb-1 justify-content-center"> \
+                <div class="card" style="width: 24rem;">\
+                    <a href="/Videos/'+href+'">\
+                        <img src="'+imgsrc+'" class="card-img-top" alt="'+title+'"> \
+                    </a>\
+                    <div class="card-body">\
+                        <h5 class="card-title">'+title+'</h5>\
+                    </div>\
+                </div>\
             </div>\
-        </div>\
-    </div>\
-    '
+            '
     
     return elem
