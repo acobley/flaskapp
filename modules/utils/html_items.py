c8e5d5d8fdb977038a29ca0fb9654ee4d722ac5c
Kamil Krauze <kamila.p.krauze@gmail.com>
2024-01-12 13:08:42 +0000

    Update html_items.py

--- modules/utils/html_items.py
+++ modules/utils/html_items.py
@@ -1,6 +1,6 @@
 def column_card(title:str, href:str, imgsrc:str) -> str:
         
-    elem = '<div class="col-sm-12 col-md-12 col-lg-3 d-flex mt-5 mb-1 justify-content-center"> \
+    elem = '<div class="col-sm-12 col-md-12 col-lg-4 col-xl-3 d-flex mt-5 mb-1 justify-content-center"> \
                 <div class="card" style="width: 24rem;">\
                     <a href="/Videos/'+href+'">\
                         <img src="'+imgsrc+'" class="card-img-top" alt="'+title+'"> \
